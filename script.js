9b805ebb3c2687b462c3eddc84ed17a33d73ec20
RobBashford <robertbashford@rocketmail.com>
2022-12-23 13:40:41 +0000

    Footer logo and social links bug fixed
    Added script to hide footer logo and social links to remove when mobile navigation is open.

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const logoCol = document.querySelector(".logo-col");
+
 const toggleMobileMenu = function (menu) {
   menu.classList.toggle("open");
+  logoCol.classList.toggle("hidden");
 };
